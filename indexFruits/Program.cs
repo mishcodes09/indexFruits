0c20144903099d0e298f742b09a7140ace6261f0
ST10085622 <st10085622@vcconnect.edu.za>
2024-07-25 20:09:21 +0200

    Updated Program.cs

--- indexFruits/Program.cs
+++ indexFruits/Program.cs
@@ -5,6 +5,15 @@
         static void Main(string[] args)
         {
             
+            Fruits fr = new Fruits("Apple", "Granny Smith", "Seeded");
+            Fruits fr2 = new Fruits("Orange", "Naartjie", "Seedless");
+
+            string dispInt = "----------- accessing using int index ----------------";
+            Console.WriteLine(dispInt + "\nFruit Type:  " + fr[0] + "\nFruit Name: " + fr[1] + "\nFruit Group: "+ fr[2]);
+
+            string dispAttrName = "\n----------- accessing using attrName index ----------------";
+            Console.WriteLine(dispAttrName + "\nFruit Type:  " + fr2["type"] + "\nFruit Name: " + fr2["name"] + "\nFruit Group: " + fr2["group"]);
+
         }
     }
 }
